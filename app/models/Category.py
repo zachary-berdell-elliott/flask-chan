71fb87d8d30d26b25308b894e73e1a2c9026abdf
zachary <zacharyberdell@gmail.com>
2022-07-30 13:42:36 -0600

    Started on the category model.

---
+++ app/models/Category.py
@@ -0,0 +1,8 @@
+import app.db as db
+import json
+from .Post import Post
+
+class Category(db.Document):
+    name = db.StringField(min_length=1, maxlength=12)
+    posts = db.ReferenceField(Post, dbref=False, reverse_delete_rule=CASCADE)
+    isNSFW = db.BooleanField(default=False)
