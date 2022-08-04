a79c402801bad20cb23b088fc3337fd6e47b595c
zachary <zacharyberdell@gmail.com>
2022-08-03 18:34:35 -0600

    Created model for comment upvotes.

--- app/models/CommentUpvote.py
+++ app/models/CommentUpvote.py
@@ -1,8 +1,8 @@
 import app.db as db
 import json
 
-class Upvote(db.Document):
+class CommentUpvote(db.Document):
     user = db.ReferenceField(User, dbref=False, reverse_delete_rule=PULL)
-    post = db.ReferenceField(Post, dbref=False, reverse_delete_rule=PULL)
+    comment = db.ReferenceField(Comment, dbref=False, reverse_delete_rule=PULL)
     upvote = db.BooleanField(required=True, default=False)
     downvote = db.BooleanField(required=True, default=False)
